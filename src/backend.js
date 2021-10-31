72d9f9a931e9c3366512bd05e60a4d6bfebb8d2d
aex <hi@aexhell.ml>
2021-10-31 17:40:58 +0100

    change backend code

--- src/backend.js
+++ src/backend.js
@@ -3,7 +3,7 @@ module.exports = () => {
    const app = express();
 
    app.get('*', async (req, res) => {
-      res.send({ status: 200, message: "Nothing here." });
+      res.send({ status: 200, code: "MAIN_HOME" });
    });
 
    app.listen(process.env.PORT || 7000);
