fa4076b5629a22ed1404d041f48f38096f66bcbc
aex <hi@aexhell.ml>
2021-11-01 00:55:16 +0100

    feat(github) ── add github command

---
+++ src/commands/github.js
@@ -0,0 +1,31 @@
+const _ = require('node-fetch');
+
+module.exports = {
+   name: 'github',
+   description: 'Get an user\'s GitHub',
+   options: [
+      {
+         name: 'target',
+         type: 3,
+         description: 'GitHub username',
+         required: true
+      }
+   ],
+   run: async (interaction) => {
+      if (interaction) {
+         let target = interaction.options.getString('target');
+         let fetched = await _(`https://api.github.com/users/${target}`);
+
+         console.log(fetched.body);
+
+         interaction.reply({
+            embeds: [
+               {
+                  title: `${fetched.body.login}'s github`,
+                  color: 'RANDOM'
+               }
+            ]
+         });
+      }
+   }
+}
