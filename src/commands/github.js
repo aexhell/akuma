99d1bb74d8fc43136912a424bce940916279be28
aex <hi@aexhell.ml>
2021-11-01 01:27:00 +0100

    feat: complete github command

--- src/commands/github.js
+++ src/commands/github.js
@@ -22,6 +22,46 @@ module.exports = {
             embeds: [
                {
                   title: `${fetched.data.login}'s github`,
+                  url: fetched.data.url,
+                  fields: [
+                     {
+                        name: ":id::",
+                        value: `\`${fetched.data.id}\`.`,
+                        inline: true
+                     },
+                     {
+                        name: 'Followers',
+                        value: fetched.data.followers ? fetched.data.followers : 'None.',
+                        inline: true
+                     },
+                     {
+                        name: 'Bio',
+                        value: fetched.data.bio ? fetched.data.bio : 'None.',
+                        inline: true
+                     },
+                     {
+                        name: 'Location',
+                        value: fetched.data.location ? fetched.data.location : 'None.',
+                        inline: true
+                     },
+                     {
+                        name: 'Joined at',
+                        value: `${fetched.data.created_at.substring(8,10)}/${fetched.data.created_at.substring(5,7)}/${fetched.data.created_at.substring(0,4)}`,
+                        inline: true
+                     },
+                     {
+                        name: 'Repositories',
+                        value: fetched.data.public_repos ? `${fetched.data.public_repos} public ${fetched.data.public_repos === 1 ? 'repository' : 'repositories'}.` : 'None.',
+                        inline: true
+                     }
+                  ],
+                  thumbnail: {
+                     url: fetched.data.avatar_url
+                  },
+                  footer: {
+                     text: "Powered by GitHub",
+                     icon_url: "https://github.com/fluidicon.png"
+                  },
                   color: 'RANDOM'
                }
             ]
