8389d5e0e661b54590b7f111da68502f40d5694e
aex <hi@aexhell.ml>
2021-11-02 01:33:26 +0100

    feat(ocr) ── add text response

--- src/commands/ocr.js
+++ src/commands/ocr.js
@@ -1,4 +1,5 @@
 const Tesseract = require('tesseract.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
    name: "ocr",
@@ -7,17 +8,33 @@ module.exports = {
       {
          name: 'url',
          type: 3,
-         description: 'Image URL'
+         description: 'Image URL',
+         required: true
       }
    ],
    run: async (interaction) => {
       if (interaction) {
+         interaction.channel.sendTyping();
+         
          Tesseract.recognize(
             interaction.options.getString('url'),
             'eng',
             { logger: m => console.log(m) }
           ).then(({ data: { text } }) => {
             console.log(text);
+            let embed = new MessageEmbed()
+               .setColor('RANDOM')
+               .setAuthor(interaction.user.tag, interaction.user.displayAvatarURL())
+               .setThumbnail(interaction.options.getString('url'))
+               .setDescription(`
+                  \`\`\`
+                  ${text}
+                  \`\`\`
+               `)
+
+            interaction.reply({
+               embeds: [embed]
+            });
           })
       }
    }
